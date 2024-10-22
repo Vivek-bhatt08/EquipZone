2c9f217b2aa4d585efb7a531d0b04ede32ecf31e
Vivek-bhatt08 <Vivekbhatt83073@gmail.com>
2024-10-22 11:06:50 +0530

    front page modified

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-// function hamBurger(){
-//     let navLink=document.getElementById('nav-center');
-//     navLink.style.display='block';
-// }
-// function colorChange(){
-//     let change=document.getElementById("zone");
-//     change.style.backgroundColor="Red";
+// let slideIndex = 0;
+// showSlides(slideIndex);
+
+// function changeSlide(n) {
+//     showSlides(slideIndex += n);
 // }
 
-let slideIndex = 0;
-showSlides(slideIndex);
-
-function changeSlide(n) {
-    showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-    const slides = document.querySelectorAll('.slide');
-    if (n >= slides.length) slideIndex = 0;
-    if (n < 0) slideIndex = slides.length - 1;
+// function showSlides(n) {
+//     const slides = document.querySelectorAll('.slide');
+//     if (n >= slides.length) slideIndex = 0;
+//     if (n < 0) slideIndex = slides.length - 1;
     
-    slides.forEach((slide, index) => {
-        slide.style.display = (index === slideIndex) ? 'block' : 'none';
-    });
-}
+//     slides.forEach((slide, index) => {
+//         slide.style.display = (index === slideIndex) ? 'block' : 'none';
+//     });
+// }
 
 function searchShown(){
     let a = document.getElementById('search');
