effe76d28a77bd7e15ce7f52ffaa2257bf1d80bc
Vivek-bhatt08 <Vivekbhatt83073@gmail.com>
2024-10-23 15:24:55 +0530

    commit

--- EquipZone Hero Page/script.js
+++
@@ -1,9 +0,0 @@
-// function hamBurger(){
-//     let navLink=document.getElementById('nav-center');
-//     navLink.style.display='block';
-// }
-// function colorChange(){
-//     let change=document.getElementById("zone");
-//     change.style.backgroundColor="Red";
-// }
-
